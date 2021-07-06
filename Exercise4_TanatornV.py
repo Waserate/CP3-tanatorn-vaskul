33cfaaee81df95870b917faecfa81c59107e56e0
Waserate <tanatorn.vask@gmail.com>
2021-07-06 17:12:05 +0700

    แบบฝึกหัดตัวแปร
    ทำแบบฝึกหัดตัวแปรเรื่องคะแนนเรียน

---
+++ Exercise4_TanatornV.py
@@ -0,0 +1,17 @@
+Eng = 90.0
+GB = 85.5
+ITCS = 94.47
+CP = 35.48
+#
+#
+print ("---------------------------")
+print ("My Score in University")
+print ("---------------------------")
+print ("Foundation English : ")
+print (Eng)
+print ("General Business : ")
+print (GB)
+print ("Introduction to Computer Systems : ")
+print (ITCS)
+print ("Computer Programming : ")
+print (CP)

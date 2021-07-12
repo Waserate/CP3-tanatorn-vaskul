7ca197aa48f6ddde862a66231e37118ef5531d35
Waserate <tanatorn.vask@gmail.com>
2021-07-12 20:32:11 +0700

    แบบฝึกหัดเรื่องฟังชั่น
    ทำฟั่งชั่นไว้ใช้ บวกลบคูณหาร

---
+++ Exercise12_tanatornV.py
@@ -0,0 +1,12 @@
+def addnumber(x,y):
+    print(x+y)
+def minusnumber(x,y):
+    print(x-y)
+def plusnumber(x,y):
+    print(x*y)
+def dividenumber(x,y):
+    print(x/y)
+addnumber(15,8)
+minusnumber(15,8)
+plusnumber(15,8)
+dividenumber(15,8)
